184edce8842f4b6bca4fab5826f9dccd770c516c
Ivanor-dev <i279746@gmail.com>
2023-11-08 18:18:57 -0300

    feat: leitura e escrita backup

--- src/Armazenamento/Predio.java
+++ src/Armazenamento/Predio.java
@@ -6,8 +6,12 @@ import java.util.ArrayList;
 
 import Posicao.Posicao;
 
+import java.io.BufferedReader;
 //import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
 //import java.io.FileOutputStream;
 //import java.io.FileReader;
@@ -83,7 +87,7 @@ public class Predio extends Produto implements Armazenamento {
 
     public void adicionarProdutoEmPosicao(Produto produto, Posicao posicao) {
         if (predioDisponivel()) {
-            if (matrizDeProdutos.get(posicao.getAndar()).get(posicao.getApartamento()) == null ) {
+            if (matrizDeProdutos.get(posicao.getAndar()).get(posicao.getApartamento()) == null) {
                 matrizDeProdutos.get(posicao.getAndar()).set(posicao.getApartamento(), produto);
             }
             System.out.println("Produto Adicionado no predio.");
@@ -124,31 +128,36 @@ public class Predio extends Produto implements Armazenamento {
         return null;
     }
 
-//    @Override
-//    public void moverProduto(Produto produto, Posicao novaLocalizacao) {
-//        // if (produtosRecebidos.contains(produto)) {
-//        // Position localizacaoAtual = produto.getPosicao();
-//        // int xAntigo = localizacaoAtual.getAndar();
-//        // int yAntigo = localizacaoAtual.getApartamento();
-//        // int xNovo = novaLocalizacao.getAndar();
-//        // int yNovo = novaLocalizacao.getApartamento();
-//        //
-//        // if (xAntigo >= 0 && xAntigo < largura && yAntigo >= 0 && yAntigo < altura &&
-//        // xNovo >= 0 && xNovo < largura && yNovo >= 0 && yNovo < altura) {
-//        // if (matrizDeProdutos[xAntigo][yAntigo] == produto) {
-//        // matrizDeProdutos[xAntigo][yAntigo] = new Produto(0, "", "", ""); // Limpar a
-//        // posição antiga
-//        // matrizDeProdutos[xNovo][yNovo] = produto;
-//        // produto.setPosicao(novaLocalizacao);
-//        // System.out.println("Produto movido para (" + novaLocalizacao.getAndar() + ",
-//        // " +
-//        // novaLocalizacao.getApartamento() + ", " + novaLocalizacao.getPredio() + ")");
-//        // }
-//        // }
-//        // } else {
-//        // System.out.println("Produto não encontrado na lista de produtos recebidos.");
-//        // }
-//    }
+    // @Override
+    // public void moverProduto(Produto produto, Posicao novaLocalizacao) {
+    // // if (produtosRecebidos.contains(produto)) {
+    // // Position localizacaoAtual = produto.getPosicao();
+    // // int xAntigo = localizacaoAtual.getAndar();
+    // // int yAntigo = localizacaoAtual.getApartamento();
+    // // int xNovo = novaLocalizacao.getAndar();
+    // // int yNovo = novaLocalizacao.getApartamento();
+    // //
+    // // if (xAntigo >= 0 && xAntigo < largura && yAntigo >= 0 && yAntigo < altura
+    // &&
+    // // xNovo >= 0 && xNovo < largura && yNovo >= 0 && yNovo < altura) {
+    // // if (matrizDeProdutos[xAntigo][yAntigo] == produto) {
+    // // matrizDeProdutos[xAntigo][yAntigo] = new Produto(0, "", "", ""); // Limpar
+    // a
+    // // posição antiga
+    // // matrizDeProdutos[xNovo][yNovo] = produto;
+    // // produto.setPosicao(novaLocalizacao);
+    // // System.out.println("Produto movido para (" + novaLocalizacao.getAndar() +
+    // ",
+    // // " +
+    // // novaLocalizacao.getApartamento() + ", " + novaLocalizacao.getPredio() +
+    // ")");
+    // // }
+    // // }
+    // // } else {
+    // // System.out.println("Produto não encontrado na lista de produtos
+    // recebidos.");
+    // // }
+    // }
 
     public boolean apartamentoDisponivel(int linha, int coluna) {
         if (linha >= 0 && linha < largura && coluna >= 0 && coluna < altura) {
@@ -167,88 +176,91 @@ public class Predio extends Produto implements Armazenamento {
                     if (matrizDeProdutos.get(i).get(j).getId().equals("0")) {
                         matrizDeProdutos.get(i).set(j, produto);
                         produto.setPosicao(novaPosicao);
-//                        quantidadeDeProdutos++;
+                        // quantidadeDeProdutos++;
                     }
                 }
             }
         }
 
     }
+
     //
-//    public void escreverBackupPredio(String id, Produto[][] matrizDeProdutos) {
-//        String arquivo = id + ".txt";
-//
-//        try (FileOutputStream arquivoBackupEscrita = new FileOutputStream(arquivo)) {
-//
-//            for (int i = 0; i < altura; i++) {
-//                for (int j = 0; j < largura; j++) {
-//                    String infoToBackup = matrizDeProdutos[i][j].getId() +
-//                            "\n" + matrizDeProdutos[i][j].getNome() +
-//                            "\n" + matrizDeProdutos[i][j].getTipoDeArmazenagem() +
-//                            "\n" + matrizDeProdutos[i][j].getDescricao() +
-//                            "\n" + String.valueOf(matrizDeProdutos[i][j].getAndar()) +
-//                            "\n" + String.valueOf(matrizDeProdutos[i][j].getApartamento()) +
-//                            "\n" + String.valueOf(matrizDeProdutos[i][j].getPredio()) +
-//                            "\n";
-//
-//                    byte[] bytes = infoToBackup.getBytes();
-//
-//                    arquivoBackupEscrita.write(bytes);
-//
-//                }
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//    //
-//     public void lerBackupPredio(String id) {
-//     String arquivo = id + ".txt";
-//
-//     try {
-//     FileReader fileReader = new FileReader(arquivo);
-//     BufferedReader buffereadReader = new BufferedReader(fileReader);
-//
-//     String[] linha = new String[7];
-//     Produto produto;
-//
-//     for (int i = 0; i < 5; i++) {
-//     while ((linha[i] = buffereadReader.readLine()) != null)
-//     ;
-//     }
-//     produto = new Produto(linha[0],
-//     linha[1],
-//     linha[2],
-//     linha[3],
-//     Integer.parseInt(linha[4]),
-//     Integer.parseInt(linha[5]),
-//     Integer.parseInt(linha[6]));
-//
-//     adicionarProduto(produto);
-//
-//     buffereadReader.close();
-//     fileReader.close();
-//     } catch (IOException e) {
-//     e.printStackTrace();
-//     }
-//     }
-//
-//     public boolean preencherEspacoVazioComProduto(Produto produto) {
-//     if (quantidadeDeProdutos < capacidadeMaxima) {
-//     for (int i = 0; i < matrizDeProdutos.size(); i++) {
-//     ArrayList<Produto> andar = matrizDeProdutos.get(i);
-//     for (int j = 0; j < andar.size(); j++) {
-//     Produto espaco = andar.get(j);
-//     if (espaco.getId().equals("0")) {
-//     andar.set(j, produto);
-//     produto.setPosicao(new Posicao(i, j, 0));
-//     quantidadeDeProdutos++;
-//     return true;
-//     }
-//     }
-//     }
-//     }
-//     return false;
-//     }
+    public void escreverBackupPredio(String id, Produto[][] matrizDeProdutos) {
+        String arquivo = id + ".txt";
+
+        try (FileOutputStream arquivoBackupEscrita = new FileOutputStream(arquivo)) {
+
+            for (int i = 0; i < altura; i++) {
+                for (int j = 0; j < largura; j++) {
+                    String infoToBackup = matrizDeProdutos[i][j].getId() +
+                            "\n" + matrizDeProdutos[i][j].getNome() +
+                            "\n" + matrizDeProdutos[i][j].getTipoDeArmazenagem() +
+                            "\n" + matrizDeProdutos[i][j].getDescricao() +
+                            "\n" + String.valueOf(matrizDeProdutos[i][j].getAndar()) +
+                            "\n" + String.valueOf(matrizDeProdutos[i][j].getApartamento()) +
+                            "\n" + String.valueOf(matrizDeProdutos[i][j].getPredio()) +
+                            "\n";
+
+                    byte[] bytes = infoToBackup.getBytes();
+
+                    arquivoBackupEscrita.write(bytes);
+
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    //
+    public void lerBackupPredio(String id) {
+        String arquivo = id + ".txt";
+
+        try {
+            FileReader fileReader = new FileReader(arquivo);
+            BufferedReader buffereadReader = new BufferedReader(fileReader);
+
+            String[] linha = new String[7];
+            Produto produto;
+
+            for (int i = 0; i < 5; i++) {
+                while ((linha[i] = buffereadReader.readLine()) != null)
+                    ;
+            }
+
+            Posicao posicao = new Posicao(Integer.parseInt(linha[4]), Integer.parseInt(linha[5]),
+                    Integer.parseInt(linha[6]));
+            produto = new Produto(linha[0],
+                    linha[1],
+                    linha[2],
+                    linha[3]);
+
+            produto.setPosicao(posicao);
+            adicionarProduto(produto);
+
+            buffereadReader.close();
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //
+    // public boolean preencherEspacoVazioComProduto(Produto produto) {
+    // if (quantidadeDeProdutos < capacidadeMaxima) {
+    // for (int i = 0; i < matrizDeProdutos.size(); i++) {
+    // ArrayList<Produto> andar = matrizDeProdutos.get(i);
+    // for (int j = 0; j < andar.size(); j++) {
+    // Produto espaco = andar.get(j);
+    // if (espaco.getId().equals("0")) {
+    // andar.set(j, produto);
+    // produto.setPosicao(new Posicao(i, j, 0));
+    // quantidadeDeProdutos++;
+    // return true;
+    // }
+    // }
+    // }
+    // }
+    // return false;
+    // }
 }
